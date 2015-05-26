26e23be2617b5eb16868c375de58d2f351dca43a
Zack Maril <zack@zacharymaril.com>
2015-05-26 19:56:19 -0400

    HAHAHA I AM KING OF THE CODECS
    YOU CANNOT CAST THE SCALES FROM YOUR EYES PSQL, FOR MY SPELLS HAVE BEEN
    CRAFTED FAR TOO FINELY AND FASTENED FAR TOO WELL FOR YOU TO OVERCOME THE
    ILLUSION I HAVE ENTRAPPED YOU WITH

--- src/cowbird/postgres.clj
+++ src/cowbird/postgres.clj
@@ -1,9 +1,12 @@
 (ns cowbird.postgres
   (:require [clojure.tools.cli :refer [parse-opts]]
-            [cowbird.protocol :refer [startup-packet]]
+            [cowbird.protocol :refer [startup-packet regular-packet]]
             [gloss.io :as gio]
+            [gloss.core :as gloss]
             [aleph.tcp :as tcp]
+            [gloss.core.formats :as formats]
             [manifold.stream :as stream]
+            [gloss.data.bytes :as bytes]
             [byte-streams :as b]
             [clojure.java.io :as io]))
 
@@ -17,10 +20,72 @@
     :id :named-parameter] ;;TODO there can be multiple of these
     ])
 
-(defonce a (atom nil))
+(defn startup-packet->map [packet]
+  (as-> (gio/decode startup-packet packet) $
+    (nth $ 2)
+    (clojure.string/split $ #"\x00")
+    (partition 2 $)
+    (mapv vec $)
+    (into {} $)))
+
+(def last-msg (atom nil))
+
+(defn handle-startup [to-client start-packet]
+  (println "Handling startup")
+  )
+
+(defn handle-regular [to-client msg]
+  (println "Handling regular")
+  )
+(defn consumer [to-client msg]
+  (reset! last-msg msg)
+  (b/print-bytes msg)
+  (if (zero? (first msg))
+    (handle-startup to-client (startup-packet->map msg))
+    (handle-regular to-client msg)))
+
+(def encode-packet  #(gio/encode regular-packet %))
+
+(defn handle-message [{:keys [sent-auth-request? id info client-info to-client] :as m} msg]
+  (println "---------------------")
+  (println "Handle message")
+  (println "Connection id " id)
+  (println "sent-auth-request?" sent-auth-request?)
+  (println to-client)
+  (b/print-bytes msg)
+  (if-not sent-auth-request?
+    (let [response {:type :auth-request
+                    :payload [12 :md5 "haha"]}]      
+      (println "Startup")
+      (println "Sent md5 password request")
+      (b/print-bytes (encode-packet response))
+      (println (stream/put! to-client (encode-packet response)))
+      (println " ")
+      (-> m
+          (assoc :client-info (startup-packet->map msg))
+          (assoc :sent-auth-request? true)))
+    (do 
+      (let [sure-ok {:type :auth-request
+                     :payload [12 :ok]}
+            response {:type :query-status
+                      :payload [5 :idle]}]
+        (println "Sure, password looks fine")
+        (b/print-bytes (encode-packet sure-ok))
+        (stream/put! to-client (encode-packet sure-ok))
+        (println "Sending idle status")
+        (b/print-bytes (encode-packet response))
+        (stream/put! to-client (encode-packet response))
+        (println "Sent okay")
+        (println "")
+        m))))
 
 (defn handler [s info]
-  (stream/consume (partial reset! a)  s))
+  (stream/reduce handle-message
+                 {:sent-auth-request? false
+                  :to-client s
+                  :info info
+                  :id (rand)}
+                 s))
 
 (defn start-server
   [port]
@@ -31,3 +96,4 @@
                  (assoc-in [:options :port] (Integer/parseInt (System/getenv "PGPORT"))))]
     (println args)
     (start-server (-> args :options :port))))
+
--- src/cowbird/protocol.clj
+++ src/cowbird/protocol.clj
@@ -1,37 +1,46 @@
 (ns cowbird.protocol
   (require [gloss.core :refer :all]
            [gloss.io   :refer :all]))
-
-;; (def regular-frame
-;;   (gloss/compile-frame
-;;    [(gloss/string :utf-8 :length 1)
-;;     (gloss/finite-frame :int32 (gloss/string :utf-8))]))
-
-(defcodec maybe-startup-packet
-  (finite-frame
-   :int32
-   (ordered-map
-    :protocol :int32
-    :payload (repeated
-              (string :utf-8 :delimiters ["\\0"])
-              :delimiters ["\\0"]))))
-
+;;Can't quite figure out how to encode this the right way. Relies on a transform
+;;afterwords.
 (defcodec startup-packet
+  [:int32 :int16 (string :utf-8)])
+
+;;https://github.com/postgres/postgres/blob/4baaf863eca5412e07a8441b3b7e7482b7a8b21a/src/include/libpq/pqcomm.h#L165
+(defcodec auth-request-codes
+  (enum :int32 {:ok 0
+                :password 3
+                :md5 5
+                :scm-creds 6
+                :gss 7
+                :gss-cnt 8
+                :sspi 9}))
+
+
+(defcodec auth-request
   (ordered-map
-   :len :int32-be
-   :protocol :int32-be
-   :payload  (repeated (string :utf-8 :delimiters ["\\0"])
-                       :prefix :none
-                       :delimiters ["\\0"]))
-                           
-  )
+   :type :auth-request
+   :payload
+   [:int32 auth-request-codes (string :utf-8 :length 4)]
+   ))
 
-;; (gloss/defcodec parameter-status
-;;   (gloss/finite-frame
-;;    :int32
-;;    [(gloss/string :utf-8) (gloss/string :utf-8)]
-;;    ))
+(defcodec query-status-codes
+  (enum :int32 {:idle \I}))
 
-;; (gio/encode startup-packet
-;;             {:protocol 3.0
-;;              :payload ["k" "v"]})
+(defcodec query-status
+  (ordered-map
+   :type :query-status
+   :payload
+   [:int32 query-status-codes]))
+
+(defcodec tag (enum :byte {:auth-request \R
+                            :query-status \Z}))
+(defcodec regular-packet
+  (header
+   tag
+   {:auth-request  auth-request
+    :query-status  query-status}
+   :type
+   ))
+
+;;(byte-streams/print-bytes  (encode regular-packet {:type :auth-request :payload [:ok ""]}))
