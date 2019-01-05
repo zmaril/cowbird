19648bc647167612354f5517208fd01b8c69e9b5
Zack Maril <zack@zacharymaril.com>
2019-01-05 14:57:59 -0500

    Working on getting the tests to run

--- run_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PATH=/home/zmaril/software/cowbird/bin:/home/zmaril/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/home/zmaril/bin
-export LD_LIBRARY_PATH=/home/zmaril/software/cowbird/resources/postgres/tmp_install/usr/local/pgsql/lib
-export PGSSLMODE=disable
-
-cd resources/postgres/src/test/regress
-./pg_regress --inputdir=. --temp-instance=./tmp_check --bindir=  --dlpath=. --schedule=./cowbird_schedule --host=localhost --use-existing
--- test/cowbird_test.exs
+++ test/cowbird_test.exs
@@ -2,7 +2,21 @@ defmodule CowbirdTest do
   use ExUnit.Case
   doctest Cowbird
 
-  test "greets the world" do
-    assert Cowbird.hello() == :world
+  test "The whole thing" do
+    test_dir = "resources/postgres/src/test/regress/"
+    {:ok, cwd} = File.cwd() 
+    pg_regress = cwd <> "/" <> test_dir <> "pg_regress"
+    System.cmd(pg_regress,
+      #These settings were holdovers from cowbird.clj, unsure what they all mean anymore, oh well.
+      ["--inputdir=.",
+       "--temp-instance=./tmp_check",
+       "--bindir=",
+       "--dlpath=.",
+       "--schedule=./cowbird_schedule",
+       "--output-dir=./test/output"
+       "--host=localhost",
+       "--use-existing"],
+      env: [{"PATH", System.get_env("PATH") <> ":" <> cwd <> "/bin"}]
+    )
   end
 end
