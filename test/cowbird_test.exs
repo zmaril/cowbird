770994d7c33b6edfb3d1625e82623f7dbcc9d3c1
Zack Maril <zack@zacharymaril.com>
2019-01-05 15:14:09 -0500

    More testing

--- test/cowbird_test.exs
+++ test/cowbird_test.exs
@@ -2,21 +2,31 @@ defmodule CowbirdTest do
   use ExUnit.Case
   doctest Cowbird
 
-  test "The whole thing" do
+  setup_all do
     test_dir = "resources/postgres/src/test/regress/"
-    {:ok, cwd} = File.cwd() 
+    {:ok, cwd} = File.cwd()
     pg_regress = cwd <> "/" <> test_dir <> "pg_regress"
-    System.cmd(pg_regress,
+    {cmd_output, exit_code} = System.cmd(pg_regress,
       #These settings were holdovers from cowbird.clj, unsure what they all mean anymore, oh well.
       ["--inputdir=.",
        "--temp-instance=./tmp_check",
        "--bindir=",
        "--dlpath=.",
        "--schedule=./cowbird_schedule",
-       "--output-dir=./test/output"
+       "--outputdir=./test/output",
        "--host=localhost",
        "--use-existing"],
-      env: [{"PATH", System.get_env("PATH") <> ":" <> cwd <> "/bin"}]
+      env: [{"PATH", System.get_env("PATH") <> ":" <> cwd <> "/bin"}],
+      stderr_to_stdout: true
     )
+    {:ok, exit_code: exit_code, cmd_output: cmd_output}
+  end
+
+  test "Exit code good", state do
+    assert state[:exit_code] == 0
+  end
+
+  test "Initdb worked", state do
+    refute String.contains?(state[:cmd_output],"pg_regress: initdb failed")
   end
 end
